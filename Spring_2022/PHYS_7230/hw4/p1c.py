47ab8921ae167a1be3195a4768e29f6684955e0f
Tien Vo <tienrvo@gmail.com>
2022-03-15 22:00:47 -0600

    Finished statmech hw4

--- Spring_2022/PHYS_7230/hw4/p1c.py
+++ Spring_2022/PHYS_7230/hw4/p1c.py
@@ -4,7 +4,7 @@ import mpl_extras as me
 import numpy as np
 
 
-k = np.logspace(-3, 5, 1000)
+k = np.logspace(-3, 6, 1000)
 f = 2 * k * np.exp(1 / 4 / k)
 
 me.setup_mpl(tex=True)
@@ -28,19 +28,21 @@ fig.savefig("p1c_1.png", dpi=600)
 plt.close(fig)
 
 
-N = 11
-T = np.logspace(-4, 4, N)
+N = 7
+T = np.logspace(-4, 2, N)
 colors = plt.cm.jet(np.linspace(0, 1, N))
-k = np.logspace(-3, 5, 1000)
+k = np.logspace(-3, 6, 1000)
 f = 2 * k * np.exp(1 / 4 / k)
 
-fig, ax = plt.subplots(1, 1)
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+#fig.subplots_adjust()
 for i in range(N):
     Fv = -0.5 + 2 * k * (np.exp(1 / 4 / k) - 1) - np.log(
         np.pi * np.sqrt(T[i] / k))
-    ax.plot(k, Fv, c=colors[i], label=fr"$T/T_\ast={T[i]}$")
+    ax.plot(
+        k, Fv, c=colors[i], label=fr"$T/T_\ast=10^{{{int(np.log10(T[i]))}}}$")
 
-#ax.legend()
+ax.legend(loc=1, frameon=False)
 ax.plot(k, f, "-k")
 ax.axhline(f.min(), c="k", ls="--")
 ax.fill_between(k, f, f.max(), color="red", alpha=0.2)
@@ -55,5 +57,6 @@ ax.tick_params(**me.params)
 ax.set_xlabel("$E_0/k_BT$")
 ax.set_ylabel(r"$\alpha/k_BT$")
 fig.savefig("p1c_2.png", dpi=600)
+plt.show()
 
 
